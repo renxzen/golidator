258b4c15aca1a431109a30fc4107632f3d8ecd11
renxzen <renxzen@gmail.com>
2023-11-04 11:10:55 -0500

    added notblank tests

---
+++ validate_test.go
@@ -0,0 +1,71 @@
+package golidator
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestNotBlankNoErrors(t *testing.T) {
+	type Request struct {
+		Field1 string  `validate:"notblank"`
+		Field2 *string `validate:"notblank"`
+	}
+
+	field2 := "testing"
+	input := Request{
+		Field1: "testing",
+		Field2: &field2,
+	}
+
+	errors, err := Validate(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := json.MarshalIndent(errors, "", "  ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(b))
+
+	if len(errors) != 0 {
+		t.Errorf("\nExpected: %v.\nResult: %v.", 0, len(errors))
+	}
+}
+
+func TestNotBlankWithErrors(t *testing.T) {
+	type Request struct {
+		Field1 string  `validate:"notblank"`
+		Field2 *string `validate:"notblank"`
+	}
+
+	field2 := ""
+	input := Request{
+		Field1: "",
+		Field2: &field2,
+	}
+
+	errors, err := Validate(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := json.MarshalIndent(errors, "", "  ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(b))
+
+	if len(errors) != 2 {
+		t.Errorf("\nExpected: %v.\nResult: %v.", 2, len(errors))
+	}
+
+	message := "Must not be blank"
+	for _, validationError := range errors {
+		for _, error := range validationError.Errors {
+			if error != message {
+				t.Errorf("Expected: %v. Result: %v", message, error)
+			}
+		}
+	}
+}
