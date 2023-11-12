6910ac6831f2e5c631ad19ad39de13de2e458a74
renxzen <renxzen@gmail.com>
2023-11-12 14:51:25 -0500

    added url error tests

--- validate_test.go
+++ validate_test.go
@@ -173,3 +173,105 @@ func TestEmailInvalidTypeError(t *testing.T) {
 		}
 	}
 }
+
+func TestUrlNoErrors(t *testing.T) {
+	type Request struct {
+		Field1 string  `validate:"url"`
+		Field2 *string `validate:"url"`
+	}
+
+	field2 := "http://renxzen.com"
+	input := Request{
+		Field1: "https://renxzen.com",
+		Field2: &field2,
+	}
+
+	errors, err := Validate(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := json.MarshalIndent(errors, "", "  ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(b))
+
+	if len(errors) != 0 {
+		t.Errorf("\nExpected: %v.\nResult: %v.", 0, len(errors))
+	}
+}
+
+func TestUrlInvalidValueError(t *testing.T) {
+	type Request struct {
+		Field1 string  `validate:"url"`
+		Field2 *string `validate:"url"`
+	}
+
+	field2 := "renxzen.com"
+	input := Request{
+		Field1: "www.renxzen",
+		Field2: &field2,
+	}
+
+	errors, err := Validate(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := json.MarshalIndent(errors, "", "  ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(b))
+
+	if len(errors) != 2 {
+		t.Errorf("\nExpected: %v.\nResult: %v.", 2, len(errors))
+	}
+
+	message := validator.URL_ERROR
+	for _, validationError := range errors {
+		for _, error := range validationError.Errors {
+			if error != message {
+				t.Errorf("Expected: %v. Result: %v", message, error)
+			}
+		}
+	}
+}
+
+func TestUrlInvalidTypeError(t *testing.T) {
+	type Request struct {
+		Field1 int  `validate:"url"`
+		Field2 *float64 `validate:"url"`
+	}
+
+	field2 := 420.0
+	input := Request{
+		Field1: 69.0,
+		Field2: &field2,
+	}
+
+	errors, err := Validate(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b, err := json.MarshalIndent(errors, "", "  ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(b))
+
+	if len(errors) != 2 {
+		t.Errorf("\nExpected: %v.\nResult: %v.", 2, len(errors))
+	}
+
+	message := validator.NOTSTRING_ERROR
+	for _, validationError := range errors {
+		for _, error := range validationError.Errors {
+			if error != message {
+				t.Errorf("Expected: %v. Result: %v", message, error)
+			}
+		}
+	}
+}
