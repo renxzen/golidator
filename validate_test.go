297bc43a05bdf5b5aca23e58b6366490cd6345f7
renxzen <renxzen@gmail.com>
2024-01-23 22:59:42 -0500

    test url table implemented

--- validate_test.go
+++ validate_test.go
@@ -156,93 +156,91 @@ func TestEmail(t *testing.T) {
 	}
 }
 
-func TestUrlOk(t *testing.T) {
+func TestUrl(t *testing.T) {
 	type Request struct {
 		Field1 string  `validate:"url"`
 		Field2 *string `validate:"url"`
 	}
 
-	field2 := "http://renxzen.com"
-	input := Request{
-		Field1: "https://renxzen.com",
-		Field2: &field2,
-	}
-
-	errors, err := Validate(input)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	LogErrorsJson(t, errors)
-
-	if len(errors) != 0 {
-		t.Errorf("\nExpected: %v.\nResult: %v.", 0, len(errors))
-	}
-}
-
-func TestUrlInvalidValueError(t *testing.T) {
-	type Request struct {
-		Field1 string  `validate:"url"`
-		Field2 *string `validate:"url"`
-	}
-
-	field2 := "renxzen.com"
-	input := Request{
-		Field1: "www.renxzen",
-		Field2: &field2,
-	}
-
-	errors, err := Validate(input)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	LogErrorsJson(t, errors)
-
-	if len(errors) != 2 {
-		t.Errorf("\nExpected: %v.\nResult: %v.", 2, len(errors))
-	}
-
-	message := validator.URL_ERROR
-	for _, validationError := range errors {
-		for _, error := range validationError.Errors {
-			if error != message {
-				t.Errorf("Expected: %v. Result: %v", message, error)
-			}
-		}
-	}
-}
-
-func TestUrlInvalidTypeError(t *testing.T) {
-	type Request struct {
+	type BadRequest struct {
 		Field1 int      `validate:"url"`
 		Field2 *float64 `validate:"url"`
 	}
 
-	field2 := 420.0
-	input := Request{
-		Field1: 69.0,
-		Field2: &field2,
+	urlOk := "http://renxzen.com"
+	urlNotOk := "renxzen.com"
+	invalidType := 420.0
+	testTable := []struct {
+		name     string
+		input    Request
+		badInput BadRequest
+		output   int
+	}{
+		{
+			name: "Ok",
+			input: Request{
+				Field1: "https://renxzen.com",
+				Field2: &urlOk,
+			},
+			output: 0,
+		},
+		{
+			name: "NotOk",
+			input: Request{
+				Field1: "www.renxzen",
+				Field2: &urlNotOk,
+			},
+			output: 2,
+		},
+		{
+			name: "InvalidType",
+			badInput: BadRequest{
+				Field1: 69.0,
+				Field2: &invalidType,
+			},
+			output: 2,
+		},
 	}
 
-	errors, err := Validate(input)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	LogErrorsJson(t, errors)
-
-	if len(errors) != 2 {
-		t.Errorf("\nExpected: %v.\nResult: %v.", 2, len(errors))
-	}
-
-	message := validator.NOTSTRING_ERROR
-	for _, validationError := range errors {
-		for _, error := range validationError.Errors {
-			if error != message {
-				t.Errorf("Expected: %v. Result: %v", message, error)
+	for _, tt := range testTable {
+		t.Run(tt.name, func(t *testing.T) {
+			var errors []ValidationError
+			var err error
+			if tt.badInput.Field1 != 0 {
+				errors, err = Validate(tt.badInput)
+			} else {
+				errors, err = Validate(tt.input)
 			}
-		}
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			LogErrorsJson(t, errors)
+
+			if len(errors) != tt.output {
+				t.Errorf("\nExpected: %v.\nResult: %v.", 0, len(errors))
+			}
+
+			if len(errors) == 0 {
+				return
+			}
+
+			message := ""
+			if tt.badInput.Field1 != 0 {
+				message = validator.NOTSTRING_ERROR
+			} else {
+				message = validator.URL_ERROR
+			}
+
+			for _, error := range errors {
+				for _, error := range error.Errors {
+					if error != message {
+						t.Errorf("Expected: %v. Result: %v", message, error)
+					}
+				}
+			}
+		})
 	}
 }
 
